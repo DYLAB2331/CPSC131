8f3864ecf1d8573557f42ad997854d2cb6dfb846
DYLAB2331 <dylannguyen2331@gmail.com>
2022-09-20 12:57:26 -0700

    Added stacks class and functions

--- Handout4/problem4.cpp
+++ Handout4/problem4.cpp
@@ -1,6 +1,50 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
+template <class T, int n>
+
+class Stack {
+
+    private:
+        int counter = 0;
+        T element[n];
+
+    public: 
+        void clearStack() {
+            counter = 0;
+        }
+
+        bool emptyStack() {
+            if (counter == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        bool fullStack() {
+            if (counter == n) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        void pushStack(T x) {
+            element[counter] = x;
+            counter++
+        }
+
+        T popStack() {
+            counter--;
+            return element[counter];
+        }
+};
+
+
+
 int main() {
-    
+
 }
