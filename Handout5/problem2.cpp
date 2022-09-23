98e7d3c11d91a4203308c8a905d6a686441db222
DYLAB2331 <dylannguyen2331@gmail.com>
2022-09-22 23:47:28 -0700

    Worked out most of problem

--- Handout5/problem2.cpp
+++ Handout5/problem2.cpp
@@ -3,54 +3,68 @@
 #include "STACKPAC.h"
 using namespace std;
 
+void isPal(string line) {
+
+    Stack <char, 80> LR;
+    Stack <char, 80> RL;
+    Stack <char, 80> temp;
+
+    LR.clear();
+    RL.clear();
+    temp.clear();
+
+    for (int i = 0; i < line.length(); i++) {
+        if (isalpha(line.at(i))) {
+            LR.push(line.at(i));
+            temp.push(line.at(i));
+        }
+    }
+
+    while (!temp.empty()) {
+        char c = temp.pop();
+        RL.push(c);
+    }
+
+    bool isPal = true;
+
+    while (!LR.empty() && isPal) {
+        char x = LR.pop();
+        char y = RL.pop();
+
+        x = tolower(x);
+        y = tolower(y);
+
+        if (x != y) {
+            isPal = false;
+        }
+    }
+
+    if (isPal == true) {
+        cout << "The phrase is a palindrome" << endl;
+    } else {
+        cout << "The phrase is not a palindrome" << endl;
+    }
+}
+
 int main() {
 
-    Stack <char, 100> LR;
-    Stack <char, 100> RL;
-    Stack <char, 100> temp;
-
-    int numPhrases;
+    string numPhrases;
     string phrase;
-    char c;
 
     cout << "How many phrases would you like to try? ";
 
     cin >> numPhrases;
 
-    for (int i = 0; i < numPhrases; i++) {
+    int n = stoi(numPhrases);
+
+    for (int i = 0; i < n; i++) {
+        cout << endl;
         cout << "Enter a phrase: ";
+
         getline(cin, phrase);
-
-        while (cin.get(c), c != '\n') {
-            if(isalpha(c)) {
-                LR.push(c);
-                temp.push(c);
-            }
-        }
-
-        while (!temp.empty()) {
-            char a = temp.pop();
-            RL.push(a);
-        }
-        
-        bool isPalindrome = true;
-
-        while (!LR.empty() && isPalindrome) {
-            char x = RL.pop();
-            char y = LR.pop();
-
-            x = tolower(x);
-            y = tolower(y);
-
-            if (x != y) {
-                isPalindrome = false;
-            }
-        }
-
-        if (isPalindrome == true) {
-            cout << "The phrase is a palindrome" << endl;
-        } else {
-            cout << "The phrase is not a palindrome" << endl;
-        }
+        isPal(phrase);
     }
+
+    return 0;
+
 }
