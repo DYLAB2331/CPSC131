7fae1455de9e7676d0e8ec04dd537fa61129c157
DYLAB2331 <dylannguyen2331@gmail.com>
2022-10-16 00:22:10 -0700

    Finished part A, displayed linked list

---
+++ Handout7/problem2.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+class LinkedList {
+    private:
+        struct node {
+            string name;
+            int age;
+            node *next;
+        };
+
+        node *list;
+    
+    public:
+        LinkedList() {
+            list = NULL;
+        }
+
+        void insert(string name, int age) {
+            node *p;
+            p = new(node);
+
+            p->name = name;
+            p->age = age;
+            p->next = NULL;
+
+            if (list == NULL) {
+                list = p;
+            } else {
+                p->next = list;
+                list = p;
+            }
+        }
+
+        void displayList() {
+            node *p;
+            p = list;
+
+            while (p != NULL) {
+                cout << p->name << "\t" << p->age << endl;
+
+                p = p->next;
+            }
+        }
+};
+
+int main() {
+    LinkedList L;
+    fstream f;
+    string fname;
+    int fage;
+
+    f.open("pro7.txt");
+
+    while (!f.eof()) {
+        f >> fname;
+        f >> fage;
+
+        L.insert(fname, fage);
+    }
+
+    L.displayList();
+}
